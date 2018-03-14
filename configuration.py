1953fea9aae2944acceb9d99c991a93061bf7c60
WuLC <liangchaowu5@gmail.com>
2018-03-14 09:32:51 +0800

    change path for different os

--- configuration.py
+++ configuration.py
@@ -1,16 +1,24 @@
 # coding:utf8
 class DefaultConfig(object):
-    env = 'main' # visdom 环境
+    env = 'CKDEF' # visdom 环境
     model = 'AlexNet' # 使用的模型，名字必须与models/__init__.py中的名字一致
     num_classes = 7
     
+    # linux path
     train_data_root = '/home/lc/FaceExpression/train/KDEF_10G/face/' # 训练集存放路径
+    train_data_root = '/home/lc/FaceExpression/train/CKPlus_10G/face/all/'
+    train_data_root = '/home/lc/FaceExpression/train/CKDEF/'
     test_data_root = '/home/lc/FaceExpression/validation/face/' # 测试集存放路径
     pretrained_model_path = None # './checkpoints/model.pth' # 加载预训练的模型的路径，为None代表不加载
 
+    # windows path
+    # train_data_root = 'F:/FaceExpression/TrainSet/CKDEF/'
+    # test_data_root = 'F:/FaceExpression/CollectedValidationSet/processed/front_regroup_oneframe/all/face/' # 测试集存放路径
+
     batch_size = 64 # batch size
     use_gpu = True # user GPU or not
-    num_workers = 4 # how many workers for loading data
+    gpu_id = '1'
+    num_workers = 6 # how many workers for loading data
     print_freq = 20 # print info every N batch
 
     debug_file = './debug' # if os.path.exists(debug_file): enter ipdb
